ae7405416f521ce68c97661d7d2aa4553c9dbacb
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-02-08 15:04:29 -0500

    mvp complete

--- smurfs/src/redux/index.js
+++ smurfs/src/redux/index.js
@@ -1,12 +1,39 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { FETCH_DATA, UPDATE_SMURFS, SET_ERROR, POST_DATA } from './actions';
 
 const initialState = {
-
+   smurfs: [],
+   isFetchingData: false,
+   error: ''
 }
 
 const reducer = (state = initialState, action) => {
    switch(action.type) {
+      case FETCH_DATA: 
+         return {
+            ...state,
+            isFetchingData: true,
+            smurfs: []
+      }
+      case UPDATE_SMURFS:
+         return {
+            ...state,
+            smurfs: action.payload,
+            isFetchingData: false
+      }
+      case SET_ERROR:
+         return {
+            ...state,
+            isFetchingData: false,
+            error: action.payload
+      }
+      case POST_DATA:
+         return {
+            ...state,
+            isFetchingData: false,
+            smurfs: action.payload
+         }
       default: return state
    }
 }
